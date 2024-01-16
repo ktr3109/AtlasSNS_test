78287782c0a8f43333cb680d31af409f571a52c9
ito_kei <k.ito@lull-inc.co.jp>
2024-01-16 22:05:00 +0900

    ログインへのredirect追加

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {return redirect('/login');});
 // Route::get('/home', 'HomeController@index')->name('home');
 
 //Auth::routes();
